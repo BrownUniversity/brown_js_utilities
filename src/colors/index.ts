9be7297508af868e75d5b7c99ac425d15774642a
Adam Holtzman <adam_holtzman@brown.edu>
2022-03-07 15:20:24 -0500

    update tsconfig file and tweaks to functions

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,28 +1,31 @@
+/* eslint-disable import/prefer-default-export */
 export function adjustColor(colorValue: string, changeAmount = -20) {
   let color = colorValue.replace(/^#/, "");
   const changePercent = changeAmount / 100;
 
   if (color.length === 3)
-    color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
+    color = `${color[0]}${color[0]}${color[1]}${color[1]}${color[2]}${color[2]}`;
 
-  // @ts-ignore
-  let [r, g, b] = color.match(/.{2}/g);
+  const r = parseInt(`${color[0]}${color[1]}`, 16);
+  const g = parseInt(`${color[2]}${color[3]}`, 16);
+  const b = parseInt(`${color[4]}${color[5]}`, 16);
 
-  [r, g, b] = [parseInt(r, 16), parseInt(g, 16), parseInt(b, 16)];
+  const newR = Math.max(
+    Math.min(255, Math.round(r + r * changePercent)),
+    0
+  ).toString(16);
+  const newG = Math.max(
+    Math.min(255, Math.round(g + g * changePercent)),
+    0
+  ).toString(16);
+  const newB = Math.max(
+    Math.min(255, Math.round(b + b * changePercent)),
+    0
+  ).toString(16);
 
-  r = Math.max(Math.min(255, Math.round(r + r * changePercent)), 0).toString(
-    16
-  );
-  g = Math.max(Math.min(255, Math.round(g + g * changePercent)), 0).toString(
-    16
-  );
-  b = Math.max(Math.min(255, Math.round(b + b * changePercent)), 0).toString(
-    16
-  );
-
-  const rr = (r.length < 2 ? "0" : "") + r;
-  const gg = (g.length < 2 ? "0" : "") + g;
-  const bb = (b.length < 2 ? "0" : "") + b;
+  const rr = (newR.length < 2 ? "0" : "") + newR;
+  const gg = (newG.length < 2 ? "0" : "") + newG;
+  const bb = (newB.length < 2 ? "0" : "") + newB;
 
   return `#${rr}${gg}${bb}`;
 }
